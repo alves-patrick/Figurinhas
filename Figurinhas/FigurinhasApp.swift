fabb7e4fdfeacf6b4057d98c2a67c8307e5031e5
Patrick Alves <patrickalvesarts@gmail.com>
2023-08-08 15:28:02 -0300

    Initial Commit

---
+++ Figurinhas/FigurinhasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigurinhasApp.swift
+//  Figurinhas
+//
+//  Created by Patrick Alves on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FigurinhasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
