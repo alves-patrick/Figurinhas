d3500f2a7f4789ef5190a26b4c14cd5ebe4966e9
Patrick Alves <patrickalvesarts@gmail.com>
2023-08-09 10:19:13 -0300

    Adicionando botões

--- Figurinhas/ContentView.swift
+++ Figurinhas/ContentView.swift
@@ -9,23 +9,89 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-           
+        VStack(alignment: .leading) {
+            
             Text("Mingle")
                 .font(.title)
-                .fontWeight(.black)
-                .foregroundColor(Color.gray)
                 .bold()
-                .textCase(/*@START_MENU_TOKEN@*/.lowercase/*@END_MENU_TOKEN@*/)
-            Text("Frase: ")
-                .bold()
+                .padding()
+            
+            HStack {
+                Text("Frase: ")
+                    .bold()
+                    .padding(.leading)
+                
+                Text("\"Problema em dobro\"")
+            }
+            
+            HStack {
+                
+                Image("figura_01")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 75)
+                
+                VStack(alignment: .leading){
+                    HStack {
+                        
+                        Image(systemName: "lifepreserver")
+                            .foregroundColor(Color.green)
+                            .frame(width: 30.0)
+                            .font(.system(size: 30))
+                        
+                        Text("Vidas: ")
+                        
+                        
+                        Text("2")
+                            .padding(.leading)
+                    }
+                    HStack {
+                        
+                        Image(systemName: "bolt")
+                            .foregroundColor(Color.yellow)
+                            .frame(width: 30.0)
+                            .font(.system(size: 30))
+                        
+                        Text("Potência: ")
+                        
+                        
+                        Text("60%")
+                    }
+                }
+                
+            }
+            .padding(30)
+            
+            HStack{
+                Spacer()
+                Text("O Mingle se destaca por fazer o dobro do trabalho na metade do tempo, com extema precisão. Essas habilidades são uteis para ela sem sua função de Analista de Dados Sênior para uma empresa internacional de computação em nuvem. Ela também tem uma propensão para dança de salão, dança de linha e praticamente qualquer tipo de atividade que a deixe dançar ao som da música.")
+                    .fontWeight(.light)
+                    .foregroundColor(Color.gray)
+                Spacer()
+            }
+            
+            Spacer()
+            
+            HStack {
+                Spacer()
+                Button {
+                    // acao
+                } label: {
+                    Text("Na sua coleção: 2")
+                        .padding()
+                        .background(.green)
+                }
+                .cornerRadius(30.0)
+                Spacer()
+                
+                
+            }.padding()
         }
-        .padding()
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
